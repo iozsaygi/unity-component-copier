e43d81820e1826cb7ef1ddc29f547abc3966f027
iozsaygi <devozsaygi@gmail.com>
2019-03-16 22:29:43 +0200

    delete

--- unity-component-copier/Assets/Scripts/ComponentCopier.cs
+++ unity-component-copier/Assets/Scripts/ComponentCopier.cs
@@ -12,49 +12,87 @@
 
         [MenuItem("GameObject/Component Copier/Copy Component(s)", validate = false, priority = 10)]
         private static void CopyComponents() {
-            AssignClickedObject();
+            clickedGameObject = GetActiveGameObject();
 
             if (clickedGameObject == null) {
                 EditorUtility.DisplayDialog("Unity Component Copier", "Can not copy components of null game object!", "Ok");
                 return;
             }
 
-            Component[] cmps = clickedGameObject.GetComponents<Component>();
+            components = Filter(clickedGameObject.GetComponents<Component>()).ToList();
 
-            if (cmps.Length == 0) {
-                EditorUtility.DisplayDialog("Unity Component Copier", "There are not enough components to copy on game object!", "Ok");
+            if (components.Count == 0) {
+                EditorUtility.DisplayDialog("Unity Component Copier", "There are not enough components to copy on game object! (Not including Transform)", "Ok");
                 return;
             }
 
-            components = cmps.ToList();
+            // @this log will not work for single thread forced components like transform.
+            Debug.Log("" + components.Count + " component(s) copied from " + clickedGameObject.name + "!");
             clickedGameObject = null;
         }
+
 
         [MenuItem("GameObject/Component Copier/Paste Component(s)")]
         private static void PasteComponents() {
             if (components.Count == 0) {
-                EditorUtility.DisplayDialog("Unity Component Copier", "There are not enough components to paste!", "Ok");
+                EditorUtility.DisplayDialog("Unity Component Copier", "There are not enough components to paste! Try to copy components again.", "Ok");
                 return;
             }
 
-            AssignClickedObject();
-
+            clickedGameObject = GetActiveGameObject();
+            
             foreach (Component component in components) {
+                // if component is added before -> remove it and paste it as new
                 ComponentUtility.CopyComponent(component);
                 ComponentUtility.PasteComponentAsNew(clickedGameObject);
+            }
+
+            Debug.Log(components.Count + " component(s) pasted on " + clickedGameObject.name + "!");
+            Reset();
+        }
+
+        [MenuItem("GameObject/Component Copier/Delete Component(s)")]
+        private static void DeleteComponents() {
+            GameObject activeGameObject = GetActiveGameObject();
+            Component[] localCmps = Filter(activeGameObject.GetComponents<Component>());
+
+            if (localCmps.Length > 0) {
+                ClearBehaviour.ClearComponents(localCmps);
+                Debug.Log(localCmps.Length + " component(s) removed from " + activeGameObject.name + "!");
+            } else {
+                EditorUtility.DisplayDialog("Unity Component Copier", "Can not delete transform component!", "Ok");
             }
 
             Reset();
         }
 
-        private static void AssignClickedObject() {
+        private static GameObject GetActiveGameObject() {
             // clicked object -> the one that is active on inspector.
-            clickedGameObject = Selection.activeGameObject;
+            return Selection.activeGameObject;
         }
 
         private static void Reset() {
             components.Clear();
             clickedGameObject = null;
+        }
+
+        private static Component[] Filter(Component[] components) {
+            return components.Where(c => c.GetType() != typeof(Transform)).ToArray();
+        }
+    }
+
+    // @this class is not for inspector usage, just contains helper function.
+    internal sealed class ClearBehaviour : MonoBehaviour {
+
+        public static void ClearComponents(Component[] components) {
+            if (components != null && components.Length > 0) {
+                foreach (Component component in components) {
+                    // trying to delete "transform" is throwing an error...
+                    if (component.GetType().ToString() != "UnityEngine.Transform") {
+                        DestroyImmediate(component);
+                    }
+                }
+            }
         }
     }
 } // namespace UnityComponentCopier
