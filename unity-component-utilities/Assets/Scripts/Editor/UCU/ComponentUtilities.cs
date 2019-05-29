74e29994b36aa7c6feebec6b24a9c4aaa3cc82df
iozsaygi <devozsaygi@gmail.com>
2019-05-29 23:06:58 +0300

    merge

--- unity-component-utilities/Assets/Scripts/Editor/UCU/ComponentUtilities.cs
+++ unity-component-utilities/Assets/Scripts/Editor/UCU/ComponentUtilities.cs
@@ -4,9 +4,13 @@ namespace UnityComponentUtilities {
     using UnityEngine;
     using UnityEditor;
     using UnityEditorInternal;
+    using System;
 
     internal static class ComponentUtilities {
         private static List<Component> copiedComponents = new List<Component>();
+        // We need this because somehow Unity calls "MenuItem" functions more than once when working with
+        // multiple game objects. Let's just hope it will be fixed with new engine versions and we'll stop doing this workaround.
+        private static float functionCallInterval = 0.0f;
 
         [MenuItem("GameObject/Component Utilities/Copy", priority = 10)]
         private static void Copy() {
@@ -49,17 +53,47 @@ namespace UnityComponentUtilities {
                 UtilityBehaviour.Clear(activeTransforms[i].GetComponents<Component>());
         }
 
+        [MenuItem("GameObject/Component Utilities/Merge")]
+        private static void Merge() {
+            if (Time.unscaledTime.Equals(functionCallInterval))
+                return;
+
+            if (Selection.transforms.Length > 1) {
+                List<Type> componentsToMerge = new List<Type>();
+
+                for (int i = 0; i < Selection.transforms.Length; i++) {
+                    Component[] components = Selection.transforms[i].GetComponents<Component>();
+
+                    for (int j = 0; j < components.Length; j++) {
+                        if (components[j] != null && components[j].GetType() != typeof(Transform))
+                            componentsToMerge.Add(components[j].GetType());
+                    }
+                }
+
+                new GameObject("Merged", componentsToMerge.ToArray());
+            }
+
+            functionCallInterval = Time.unscaledTime;
+        }
+
         [MenuItem("GameObject/Component Utilities/Separate")]
         private static void Separate() {
-            // Will not work when multiple game objects are selected in editor.
-            // Waiting for new engine versions to fix.
-            foreach (Component component in Selection.activeGameObject.GetComponents<Component>()) {
-                if (component != null && component.GetType() != typeof(Transform)) {
-                    GameObject gameObject = new GameObject(component.GetType().Name);
-                    ComponentUtility.CopyComponent(component);
-                    ComponentUtility.PasteComponentAsNew(gameObject);
+            if (Time.unscaledTime.Equals(functionCallInterval))
+                return;
+
+            for (int i = 0; i < Selection.transforms.Length; i++) {
+                Component[] components = Selection.transforms[i].GetComponents<Component>();
+
+                for (int j = 0; j < components.Length; j++) {
+                    if (components[j] != null && components[j].GetType() != typeof(Transform)) {
+                        GameObject gameObject = new GameObject(components[j].GetType().Name);
+                        ComponentUtility.CopyComponent(components[j]);
+                        ComponentUtility.PasteComponentAsNew(gameObject);
+                    }
                 }
             }
+
+            functionCallInterval = Time.unscaledTime;
         }
 
         // To destroy components from game objects i needed to call "DestroyImmediate" function
