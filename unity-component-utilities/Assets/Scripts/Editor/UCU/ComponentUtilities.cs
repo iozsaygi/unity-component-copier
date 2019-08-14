dfafe2769f736f28eb668fd957e900b0ebbf3272
iozsaygi <devozsaygi@gmail.com>
2019-08-14 18:31:47 +0300

    v1.6

--- unity-component-utilities/Assets/Scripts/Editor/UCU/ComponentUtilities.cs
+++ unity-component-utilities/Assets/Scripts/Editor/UCU/ComponentUtilities.cs
@@ -1,10 +1,11 @@
 ﻿#if UNITY_EDITOR
 namespace UnityComponentUtilities {
+    using System;
     using System.Collections.Generic;
-    using UnityEngine;
+    using System.Linq;
     using UnityEditor;
     using UnityEditorInternal;
-    using System;
+    using UnityEngine;
 
     internal static class ComponentUtilities {
         private static List<Component> copiedComponents = new List<Component>();
@@ -13,10 +14,15 @@ namespace UnityComponentUtilities {
         // So this variable is here to make sure that we are running the code only once.
         private static float functionTriggerInterval = 0.0f;
 
-        [MenuItem("GameObject/Component Utilities/Copy", priority = 10)]
+        [MenuItem("GameObject/Component Utilities/Common/Copy", priority = 10)]
         private static void Copy() {
             copiedComponents.Clear();
             Transform[] activeTransforms = Selection.transforms;
+
+            if (activeTransforms.Length == 0) {
+                EditorUtility.DisplayDialog("Nothing selected!", "Please select at least 1 GameObject to copy components from!", "Ok");
+                return;
+            }
 
             for (int i = 0; i < activeTransforms.Length; i++) {
                 Component[] componentsOfSelectedTransforms = activeTransforms[i].GetComponents<Component>();
@@ -29,12 +35,20 @@ namespace UnityComponentUtilities {
             }
         }
 
-        [MenuItem("GameObject/Component Utilities/Paste")]
-        private static void Paste() {
+        [MenuItem("GameObject/Component Utilities/Common/Paste/On GameObject(s)")]
+        private static void PasteOnGameObject() {
             Transform[] activeTransforms = Selection.transforms;
 
-            if (copiedComponents.Count == 0)
+            if (activeTransforms.Length == 0) {
+                EditorUtility.DisplayDialog("Nothing selected!", "Please select at least 1 GameObject to paste copied components!", "Ok");
+                copiedComponents.Clear();
                 return;
+            }
+
+            if (copiedComponents.Count == 0) {
+                EditorUtility.DisplayDialog("Nothing copied!", "Not enough components are copied for pasting! Copy at least 1 component.", "Ok");
+                return;
+            }
 
             for (int i = 0; i < activeTransforms.Length; i++) {
                 for (int j = 0; j < copiedComponents.Count; j++) {
@@ -46,15 +60,59 @@ namespace UnityComponentUtilities {
             copiedComponents.Clear();
         }
 
-        [MenuItem("GameObject/Component Utilities/Delete")]
+        [MenuItem("GameObject/Component Utilities/Common/Paste/As Child")]
+        private static void PasteAsChild() {
+            bool result = EditorUtility.DisplayDialog("Choose pasting mode!", "Do you want to create new game object for each copied component?", "Yes", "No");
+            Transform[] activeTransforms = Selection.transforms;
+
+            switch (result) {
+                case true:
+                    for (int i = 0; i < activeTransforms.Length; i++) {
+                        for (int j = 0; j < copiedComponents.Count; j++) {
+                            GameObject separatedChild = new GameObject("Pasted Component");
+                            separatedChild.transform.SetParent(activeTransforms[i], false);
+                            ComponentUtility.CopyComponent(copiedComponents[j]);
+                            ComponentUtility.PasteComponentAsNew(separatedChild);
+                        }
+                    }
+                    break;
+
+                case false:
+                    GameObject gameObject = new GameObject("Pasted Component");
+
+                    for (int i = 0; i < activeTransforms.Length; i++) {
+                        gameObject.transform.SetParent(activeTransforms[i], false);
+
+                        for (int j = 0; j < copiedComponents.Count; j++) {
+                            ComponentUtility.CopyComponent(copiedComponents[j]);
+                            ComponentUtility.PasteComponentAsNew(gameObject);
+                        }
+                    }
+                    break;
+            }
+
+            copiedComponents.Clear();
+        }
+
+        [MenuItem("GameObject/Component Utilities/Common/Delete")]
         private static void Delete() {
             Transform[] activeTransforms = Selection.transforms;
+
+            if (activeTransforms.Length == 0) {
+                EditorUtility.DisplayDialog("Nothing selected!", "Please select at least 1 GameObject to delete component(s)", "Ok");
+                return;
+            }
 
             for (int i = 0; i < activeTransforms.Length; i++)
                 UtilityBehaviour.Clear(activeTransforms[i].GetComponents<Component>());
         }
 
         private static void Merge(Component[] components) {
+            if (components.Length == 0) {
+                EditorUtility.DisplayDialog("Nothing selected!", "Please select at least 1 GameObject to perform merge!", "Ok");
+                return;
+            }
+
             Type[] componentsToMerge = new Type[components.Length];
 
             for (int i = 0; i < components.Length; i++) {
@@ -103,6 +161,11 @@ namespace UnityComponentUtilities {
         }
 
         private static void Separate(Component[] components) {
+            if (components.Length == 0) {
+                EditorUtility.DisplayDialog("Nothing selected!", "Please select at least 1 GameObject to separate components!", "Ok");
+                return;
+            }
+
             for (int i = 0; i < components.Length; i++) {
                 if (components[i] != null && components[i].GetType() != typeof(Transform)) {
                     GameObject gameObject = new GameObject(components[i].GetType().Name);
@@ -149,6 +212,35 @@ namespace UnityComponentUtilities {
 
             Separate(components.ToArray());
             UtilityBehaviour.Clear(transformsToDelete);
+        }
+
+        [MenuItem("GameObject/Component Utilities/Sort/By Name")]
+        private static void SortByName() {
+            if (Time.unscaledTime.Equals(functionTriggerInterval))
+                return;
+
+            Transform[] activeTransforms = Selection.transforms;
+
+            if (activeTransforms.Length == 0) {
+                EditorUtility.DisplayDialog("Nothing selected!", "Please select at least 1 GameObject to sort it's components!", "Ok");
+                return;
+            }
+
+            if (EditorUtility.DisplayDialog("Warning!", "This action will cause data loss on components! Do you want to perform this action?", "Yes", "No")) {
+                for (int i = 0; i < activeTransforms.Length; i++) {
+                    Component[] components = activeTransforms[i].GetComponents<Component>().Where(x => x.GetType() != typeof(Transform)).ToArray();
+                    List<Component> sortedComponents = components.ToList();
+
+                    sortedComponents.Sort((x, y) => x.GetType().Name.CompareTo(y.GetType().Name));
+
+                    UtilityBehaviour.Clear(components);
+
+                    foreach (Component component in sortedComponents)
+                        activeTransforms[i].gameObject.AddComponent(component.GetType());
+                }
+
+                functionTriggerInterval = Time.unscaledTime;
+            }
         }
 
         // To destroy components/objects i needed to call "DestroyImmediate" function
